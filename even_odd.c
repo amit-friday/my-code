7710d563c4b5c0bc3ae9637196b0c460f0bf7a5b
amit kumar <70883629+amit-friday@users.noreply.github.com>
2021-11-22 19:39:58 +0530

    it's check the value is either even or odd

---
+++ even_odd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// this is a program checking whether entered number is divisible by 2 or not....
+int main(){
+	//declaring value
+	int lan;
+	printf("Enter a number :- ");
+	
+	// Taking input through user
+	scanf("%d",&lan);
+	
+	//checking value through condition ...........
+	
+	if (lan%2==0)
+		printf("Number entered is Even");
+	else
+		printf("Number is ODD");
+	
+	return 0;
+	
+
+}
