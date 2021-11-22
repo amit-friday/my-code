8c1c29879a30df90f5efa10d70375fa39dbc35d6
amit kumar <70883629+amit-friday@users.noreply.github.com>
2021-11-22 20:17:01 +0530

    Update even_odd.c

--- even_odd.c
+++ even_odd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 // this is a program checking whether entered number is divisible by 2 or not....
 int main(){
+	// 1st method ................................................................................................................................................
 	//declaring value
 	int lan;
 	printf("Enter a number :- ");
@@ -14,6 +15,19 @@ int main(){
 		printf("Number entered is Even");
 	else
 		printf("Number is ODD");
+	
+	// 2nd method to solve this..................................................................................................................................
+	
+	//declaring value
+	int lan;
+	printf("Enter a number :- ");
+	
+	// Taking input through user
+	scanf("%d",&lan);
+	
+	//checking value through conditional operator........... ...........
+	(lan%2==0 ? printf("Number entered is Even"):printf("Number is ODD"));
+	
 	
 	return 0;
 	
